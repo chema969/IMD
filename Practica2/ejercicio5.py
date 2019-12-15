e9f0f834121f462eacc603d7764408f05643ca3a
José Manuel Cuevas Muñoz <33511318+chema969@users.noreply.github.com>
2019-12-15 23:12:18 +0100

    xd

--- Practica2/ejercicio5.py
+++ Practica2/ejercicio5.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from sklearn import preprocessing
-
+from pkg_resources import parse_version
 le = preprocessing.LabelEncoder()
-nombre_variables = ['viento', 'velocidad_viento', 'periodo_ola_dominante', 'media_ola_dominante','presion','temperatura','clase']
+nombre_variables = ['viento', 'v_viento', 'p_ola', 'm_ola','presion','temperatura']
 olas = pd.read_csv('train.csv')
 le.fit(olas['clase'])
 clases_numeros = le.transform(olas['clase'])
 
 olas_df = pd.DataFrame(olas[nombre_variables], columns=nombre_variables)
 
-pd.plotting.scatter_matrix(olas_df, c=clases_numeros, figsize=(8, 8));
+pd.plotting.scatter_matrix(olas_df, c=clases_numeros,diagonal='kde', figsize=(15, 15),grid=False);
